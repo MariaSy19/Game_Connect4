983cbf1bde692bdc01c6044f405d0184d5c3268b
عبدالرحمن نصر عقل خليل <102249752+AbdoAqll@users.noreply.github.com>
2023-06-30 22:39:33 +0300

    this is updated version of the game
    the game now works put needs some functions the first one is check draw and complete the checkWin function to check for horizontal rows and vertical columns

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import connect_4.ConnectFour
+fun main()
+{
+    var game : ConnectFour = ConnectFour()
+    game.gamePlay()
+}
---
+++ src/main/kotlin/connect_4/ConnectFour.kt
@@ -0,0 +1,121 @@
+package connect_4
+
+class ConnectFour {
+    val board: Array<Array<String>> = Array(6) { Array(7) { " " } }
+    var gameOver: Boolean = false
+    var c: Char = 'X'
+    fun checkWin(): Boolean {
+        for (row in 0..3) {
+            for (col in 0..2) {
+                if (board[row][col] != " " &&
+                    board[row][col] == board[row + 1][col + 1] &&
+                    board[row][col] == board[row + 2][col + 2] &&
+                    board[row][col] == board[row + 3][col + 3]
+                ) {
+                    return true
+                }
+            }
+        }
+        for (row in 0..3) {
+            for (col in 6 downTo 3) {
+                if (board[row][col] != " " &&
+                    board[row][col] == board[row + 1][col - 1] &&
+                    board[row][col] == board[row + 2][col - 2] &&
+                    board[row][col] == board[row + 3][col - 3]
+                ) {
+                    return true
+                }
+
+            }
+        }
+        return false
+    }
+
+    fun checkDraw() : Boolean
+    {
+        // write the code of check draw function here
+        // the true return value is to avoid the error only you can delete it
+        return true
+    }
+
+    fun printBoard() {
+        val numRows = board.size
+        val numCols = board[0].size
+
+        // Print column numbers
+        for (col in 1..numCols) {
+            print("   $col  ")
+        }
+        println()
+
+        // Print board with boundaries
+        for (row in 0 until numRows) {
+            // Print top boundary for each row
+            for (col in 0 until numCols) {
+                print("______")
+            }
+            println()
+
+            // Print cell values and number
+            for (col in 0 until numCols) {
+                print("|  ${board[row][col]}  ")
+            }
+            println("|")
+        }
+
+        // Print bottom boundary for the last row
+        for (col in 0 until numCols) {
+            print("______")
+        }
+        println()
+        println()
+    }
+
+    fun gamePlay() {
+        val rows = 6
+        val cols = 7
+        var currentPlayer = "X"
+
+        while (true) {
+
+            // Print board
+            printBoard()
+
+            // Get column choice from the current player
+            println("Player $currentPlayer's turn.")
+            print("Enter column (1-$cols): ")
+            val column = readlnOrNull()?.toIntOrNull()
+
+            if (column != null && column in 1..cols) {
+                // Place the current player's piece in the chosen column
+                for (row in rows - 1 downTo 0) {
+                    if (board[row][column - 1] == " ") {
+                        board[row][column - 1] = currentPlayer
+                        break
+                    }
+                }
+
+                // Check if the current player wins
+                if (checkWin()) {
+                    // Print final board
+                    printBoard()
+                    println("Player $currentPlayer wins!")
+                    break
+                }
+
+                // Check if it's a draw
+                if (checkDraw()) {
+                    // Print final board
+                    printBoard()
+                    println("It's a draw!")
+                    break
+                }
+
+                // Switch to the other player
+                currentPlayer = if (currentPlayer == "X") "O" else "X"
+            } else {
+                println("Invalid column choice. Please try again.")
+            }
+        }
+    }
+}
